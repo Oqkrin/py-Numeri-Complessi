63c02ff7568e2a60aa12684764aec8da8d8f0e60
zeDekuX <iona.paolo05@gmail.com>
2023-02-02 15:43:16 +0100

    modificato pulsante

--- InterfacciaGrafica.py
+++ InterfacciaGrafica.py
@@ -28,6 +28,6 @@ label1.grid(row=0, column=0)
 label2.grid(row=1, column=0)
 entry1.grid(row=0, column=1)
 entry2.grid(row=1, column=1)
-button.grid(row=2, column=0)
+button.grid(row=2, column=1)
 
 root.mainloop()
